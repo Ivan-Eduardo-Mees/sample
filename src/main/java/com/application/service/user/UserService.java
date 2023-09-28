d1f6d4248e97d244190084b8a7565b9019b47dd1
mees.ivan <mees.ivan@hotmail.com>
2023-09-28 18:40:19 -0300

    Alterando nome pasta entity -> model

--- src/main/java/com/application/service/user/UserService.java
+++ src/main/java/com/application/service/user/UserService.java
@@ -1,10 +1,7 @@
 package com.application.service.user;
 
-import com.application.entity.user.User;
-import com.application.entity.user.UserRole;
 import com.application.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 @Service
